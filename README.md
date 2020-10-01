06f13e49b1711537bc85e56e2c5c13a7d4be688a
RikoGit <r.git@lenta.ru>
2020-10-01 03:05:53 +0300

    Next.js app on Heroku

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://nextjs-material-example.herokuapp.com/
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+export default () => (
+  <div className="root">
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Nextjs-material-example</title>
+    </Head>
+    <p>Test</p>
+  </div>
+);
