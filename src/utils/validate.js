3bf7cd74504d0adc9c2c58e824f9bc7321c5f1a3
RikoGit <r.git@lenta.ru>
2020-10-04 20:16:09 +0300

    test

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,9 +2,21 @@ const validate = (value, { type, validatePattern }) => {
   if (!value) return true;
 
   if (type === "name") {
+    if (/^[а-яёА-ЯЁa-zA-Z ]*$/.test(value)) {
+      return true;
+    }
+    return false;
   }
 
   if (type === "mail") {
+    if (
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        value
+      )
+    ) {
+      return true;
+    }
+    return false;
   }
 
   if (type === "phone") {
